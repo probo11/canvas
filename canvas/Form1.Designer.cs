868f8895f6500561c2d4ed352c929f80d7be3672
sander <sanderbeijaard@hotmail.nl>
2019-03-01 12:08:47 +0100

    paint shapes

--- canvas/Form1.Designer.cs
+++ canvas/Form1.Designer.cs
@@ -28,13 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.rectButton = new System.Windows.Forms.Button();
+            this.elipButton = new System.Windows.Forms.Button();
+            this.canvas = new System.Windows.Forms.PictureBox();
+            this.seleButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rectButton
+            // 
+            this.rectButton.Location = new System.Drawing.Point(12, 16);
+            this.rectButton.Name = "rectButton";
+            this.rectButton.Size = new System.Drawing.Size(75, 23);
+            this.rectButton.TabIndex = 1;
+            this.rectButton.Text = "Rectangle";
+            this.rectButton.UseVisualStyleBackColor = true;
+            this.rectButton.Click += new System.EventHandler(this.rectButton_Click);
+            // 
+            // elipButton
+            // 
+            this.elipButton.Location = new System.Drawing.Point(93, 16);
+            this.elipButton.Name = "elipButton";
+            this.elipButton.Size = new System.Drawing.Size(75, 23);
+            this.elipButton.TabIndex = 2;
+            this.elipButton.Text = "Elipse";
+            this.elipButton.UseVisualStyleBackColor = true;
+            this.elipButton.Click += new System.EventHandler(this.elipButton_Click);
+            // 
+            // canvas
+            // 
+            this.canvas.Location = new System.Drawing.Point(12, 45);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(1297, 567);
+            this.canvas.TabIndex = 3;
+            this.canvas.TabStop = false;
+            this.canvas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseDown);
+            this.canvas.MouseUp += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseUp);
+            // 
+            // seleButton
+            // 
+            this.seleButton.Location = new System.Drawing.Point(174, 16);
+            this.seleButton.Name = "seleButton";
+            this.seleButton.Size = new System.Drawing.Size(75, 23);
+            this.seleButton.TabIndex = 4;
+            this.seleButton.Text = "Select";
+            this.seleButton.UseVisualStyleBackColor = true;
+            this.seleButton.Click += new System.EventHandler(this.seleButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1321, 624);
+            this.Controls.Add(this.seleButton);
+            this.Controls.Add(this.canvas);
+            this.Controls.Add(this.elipButton);
+            this.Controls.Add(this.rectButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.Button rectButton;
+        private System.Windows.Forms.Button elipButton;
+        private System.Windows.Forms.PictureBox canvas;
+        private System.Windows.Forms.Button seleButton;
     }
 }
 
--- canvas/Form1.cs
+++ canvas/Form1.cs
@@ -12,9 +12,88 @@ namespace canvas
 {
     public partial class Form1 : Form
     {
+        Boolean paint = false;
+        int bob = 0;
+
+        List<Point> points = new List<Point>();
+        List<int> shapes = new List<int>();
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void rectButton_Click(object sender, EventArgs e)
+        {
+            bob = 0;
+        }
+
+        private void elipButton_Click(object sender, EventArgs e)
+        {
+            bob = 1;
+        }
+
+        private void seleButton_Click(object sender, EventArgs e)
+        {
+            bob = 2;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            canvas.BackColor = Color.White;
+            canvas.Paint += new PaintEventHandler(this.canvas_Paint);
+
+            this.Controls.Add(canvas);
+        }
+
+        private void canvas_Paint(object sender, PaintEventArgs e)
+        {
+            if (paint)
+            {
+                Graphics g = e.Graphics;
+                Pen pen = new Pen(Color.Black);
+                int i = 0;
+                int j = 0;
+
+                foreach (var item in points)
+                {
+                    if (i%2 ==0)
+                    {
+                        switch (shapes[j])
+                        {
+                            case 0:
+                                g.DrawRectangle(pen, points[i].X, points[i].Y, points[i + 1].X - points[i].X, points[i + 1].Y - points[i].Y);
+                                break;
+                            case 1:
+                                //g.DrawLine(pen, points[i], points[i + 1]);
+                                g.DrawEllipse(pen, points[i].X, points[i].Y, points[i + 1].X - points[i].X, points[i + 1].Y - points[i].Y);
+                                break;
+                            case 2:
+                                //selecteer
+                                break;
+                        }
+                        j++;
+                    }
+                    i++;
+                }
+            }
+        }
+
+        private void canvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            paint = true;
+            points.Add(e.Location);
+        }
+
+        private void canvas_MouseUp(object sender, MouseEventArgs e)
+        {
+            points.Add(e.Location);
+            shapes.Add(bob);
+            canvas.Refresh();
+            paint = false;
+
+        }
+
+
     }
 }
